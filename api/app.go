275af99270a91d9fe318ae05d160680c4c29cab7
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2020-02-29 16:41:26 +0200

    Add create users endpoint

--- api/app.go
+++ api/app.go
@@ -2,26 +2,50 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 	cors "github.com/rs/cors/wrapper/gin"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/prutya/todoapp-go/internal/app_config"
 )
 
+type User struct {
+	Id             uuid.UUID
+	Login          string
+	PasswordDigest string
+	Locale         string
+	Roles          []string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+type Todo struct {
+	Id        uuid.UUID
+	UserId    uuid.UUID
+	Body      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type UserDTOCreateInput struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+type UserDTO struct {
+	Id     uuid.UUID `json:"id"`
+	Login  string    `json:"login"`
+	Locale string    `json:"locale"`
+	Roles  []string  `json:"roles"`
+}
+
 func main() {
 	config := app_config.New()
-
-	router := gin.Default()
-	router.Use(cors.Default())
-
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
 
 	db, err := sql.Open("postgres", config.DatabaseUrl)
 
@@ -37,34 +61,92 @@ func main() {
 		panic(err)
 	}
 
-	sqlStatement := `
-		SELECT login, password_digest, locale, created_at, updated_at
-		FROM users
-		WHERE id = $1
-	`
+	router := gin.Default()
+	router.Use(cors.Default())
 
-	var login string
-	var password_digest string
-	var locale string
-	var created_at time.Time
-	var updated_at time.Time
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
 
-	row := db.QueryRow(sqlStatement, "4ac5d83a-7f39-486e-aebb-8fdd81fdb29c")
-	err = row.Scan(&login, &password_digest, &locale, &created_at, &updated_at)
+	router.POST("/users", func(ctx *gin.Context) {
+		var inputJson UserDTOCreateInput
 
-	switch err {
-	case sql.ErrNoRows:
-		fmt.Println("No rows were returned!")
-		return
-	case nil:
-		fmt.Println(login)
-		fmt.Println(password_digest)
-		fmt.Println(locale)
-		fmt.Println(created_at)
-		fmt.Println(updated_at)
-	default:
-		panic(err)
-	}
+		if err := ctx.ShouldBindJSON(&inputJson); err != nil {
+			// TODO: Hide Go error
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		row := db.QueryRow(`SELECT 1 FROM users WHERE login=$1;`, inputJson.Login)
+
+		var userExists int
+		switch err = row.Scan(&userExists); err {
+		case sql.ErrNoRows:
+		case nil:
+			ctx.JSON(http.StatusConflict, gin.H{})
+			return
+		default:
+			panic(err)
+		}
+
+		var passwordDigest, err = bcrypt.GenerateFromPassword(
+			[]byte(inputJson.Password),
+			14,
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		var user UserDTO
+		row = db.QueryRow(`
+			INSERT INTO users (login, password_digest)
+			VALUES ($1, $2)
+			RETURNING id, login, locale, roles
+		`, inputJson.Login, passwordDigest)
+
+		err = row.Scan(&user.Id, &user.Login, &user.Locale, pq.Array(&user.Roles))
+
+		if err != nil {
+			panic(err)
+		}
+
+		ctx.JSON(http.StatusCreated, user)
+	})
+
+	router.GET("/users/:id", func(ctx *gin.Context) {
+		// TODO: Authenticate
+		// TODO: Find a user by id if it is the current user
+	})
+
+	router.POST("/sessions", func(ctx *gin.Context) {
+		// TODO: Create a session
+	})
+
+	router.POST("/todos", func(ctx *gin.Context) {
+		// TODO: Authenticate
+		// TODO: Create a todo for current user
+	})
+
+	router.GET("/todos", func(ctx *gin.Context) {
+		// TODO: Authenticate
+		// TODO: Return todos belonging to the current user
+	})
+
+	router.GET("/todos/:id", func(ctx *gin.Context) {
+		// TODO: Authenticate
+		// TODO: Find a todo by id if it belongs to the current user
+	})
+
+	router.PATCH("/todos/:id", func(ctx *gin.Context) {
+		// TODO: Authenticate
+		// TODO: Update a todo by id if it belongs to the current user
+	})
+
+	router.DELETE("/todos/:id", func(ctx *gin.Context) {
+		// TODO: Authenticate
+		// TODO: Delete a todo by id if it belongs to the current user
+	})
 
 	router.Run()
 }
--- api/go.mod
+++ api/go.mod
@@ -4,6 +4,9 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.5.0
+	github.com/google/uuid v1.1.1
+	github.com/jinzhu/gorm v1.9.12
 	github.com/lib/pq v1.3.0
 	github.com/rs/cors v1.7.0
+	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
 )
